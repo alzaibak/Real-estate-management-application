82d2687fec140cc3e736db79e56c640f38d89691
alzaibak <alzaibak.abdalaziz@gmail.com>
2023-03-12 12:25:28 +0100

    Maven javaFx project creating

---
+++ Real-estate-management-application/src/main/java/com/alzaibak/Real_estate_management_application/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.alzaibak.Real_estate_management_application;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
