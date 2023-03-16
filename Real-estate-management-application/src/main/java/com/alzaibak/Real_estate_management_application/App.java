dd25d7c2af00910392bef51dde8c65fa8ca25344
alzaibak <alzaibak.abdalaziz@gmail.com>
2023-03-16 21:17:14 +0100

    Main page opening in the main app

--- Real-estate-management-application/src/main/java/com/alzaibak/Real_estate_management_application/App.java
+++ Real-estate-management-application/src/main/java/com/alzaibak/Real_estate_management_application/App.java
@@ -4,35 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/interfaces/Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.setFill(Color.TRANSPARENT);
+			//scene.getStylesheets().add(getClass().getResource("App.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.show();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+	}
+    
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
+
+
+	
 
 }
